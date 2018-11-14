57a12c65c94139b5805d2aa1ffa461240056de2f
Jonathan Yeung <yo@fullspeed.co.jp>
2018-11-14 12:02:03 +0900

    app-engine.yamlはapp.yamlに変更

--- app/app.go
+++ app/app.go
@@ -1,7 +1,24 @@
+// +build appengine
+
 package app
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+)
 
 // reference our echo instance and create it early
 var e = createMux()
+
+func createMux() *echo.Echo {
+	e := echo.New()
+
+	// note: we don't need to provide the middleware or static handlers, that's taken care of by the platform
+	// app engine has it's own "main" wrapper - we just need to hook echo into the default handler
+	http.Handle("/", e)
+	return e
+}
 
 func Start() {
 	e.Logger.Fatal(e.Start(":8080"))
